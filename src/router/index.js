da4f6d7302e6b47d040046bb02e37baac1a8ecbd
Adam Ettenberger <adam.ettenberger@gmail.com>
2025-06-03 08:00:24 -0700

    Add route stubs

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,37 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
 import Home from '/src/views/Home.vue'
+import match_three from '/src/views/project/match_three.vue'
+import renu from '/src/views/project/renu.vue'
+import webgl_metaballs from '/src/views/project/webgl_metaballs.vue'
+import webgl_flocking from '/src/views/project/webgl_flocking.vue'
+import webgl_example from '/src/views/project/webgl_example.vue'
 import { createWebHashHistory } from 'vue-router';
 
 const routes = [
   {
     path: "/",
-    name: 'Home',
     component: Home,
+  },
+  {
+    path: "/project/match_three",
+    component: match_three,
+  },
+  {
+    path: "/project/renu",
+    component: renu,
+  },
+  {
+    path: "/project/webgl_metaballs",
+    component: webgl_metaballs,
+  },
+  {
+    path: "/project/webgl_flocking",
+    component: webgl_flocking,
+  },
+  {
+    path: "/project/webgl_example",
+    component: webgl_example,
   },
 ];
 const router = createRouter({
