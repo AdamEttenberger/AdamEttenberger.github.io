fb5ef763e1799a1740d5752d2002ab852f3de43a
Adam Ettenberger <adam.ettenberger@gmail.com>
2025-06-05 11:03:42 -0700

    Update player to load on demand unless explicitly enabled

--- src/components/player.vue
+++ src/components/player.vue
@@ -1,28 +1,62 @@
 <script setup>
+import { ref } from 'vue';
+
 const props = defineProps({
   title: { type: String, required: true },
   frame: { type: String, required: true },
+  aspect: { type: String, default: "4 / 3" },
+  paused: { type: Boolean, default: true },
 })
+
+const requested_play = ref(false);
 </script>
 
 <template>
-  <div class="player">
+  <div v-if="!requested_play && paused" class="player">
+    <button class="play-button" @click.once="requested_play = true"><font-awesome-icon :icon="['fas', 'circle-play']" /></button>
+  </div>
+  <div v-else class="player">
     <iframe class="renderer" :title="title" :src="frame"></iframe>
   </div>
 </template>
 
 <style scoped>
 .player {
-  display: flex;
-  flex-direction: column;
-  align-self: stretch;
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: auto;
+  aspect-ratio: v-bind(aspect);
+
+  & > * {
+    grid-row: 1;
+    grid-column: 1;
+  }
+
+  & > button {
+    font-size: 4rem;
+    margin: 0;
+    padding: 0;
+    border: none;
+    color: var(--color-link);
+    transition: color var(--anim-transition);
+    background-color: #000;
+    cursor: pointer;
+    &:hover {
+      color: var(--color-link-hover);
+      background-color: #151515;
+    }
+    &:active {
+      color: var(--color-link-active);
+      background-color: #232323;
+    }
+  }
+
   border: 6px solid var(--color-divider);
   user-select: none;
 
   & > .renderer {
-    aspect-ratio: var(--size-player-aspect);
-    max-width: var(--size-player-max-width);
-    max-height: var(--size-player-max-height);
+    aspect-ratio: v-bind(aspect);
+    place-self: stretch;
     border: none;
     margin: 0;
     padding: 0;
--- src/main.js
+++ src/main.js
@@ -8,14 +8,30 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 // import { fas } from '@fortawesome/free-solid-svg-icons'
 // import { far } from '@fortawesome/free-regular-svg-icons'
 // import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faServer, faCloudArrowUp, faCloudArrowDown } from '@fortawesome/free-solid-svg-icons'
-import { faLinkedin, faGithubSquare } from '@fortawesome/free-brands-svg-icons'
+import {
+  faCirclePlay,
+  faCloudArrowUp,
+  faCloudArrowDown,
+  faServer,
+} from '@fortawesome/free-solid-svg-icons'
+import {
+  faGithubSquare,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 const app = createApp(App);
 // FontAwesome Config
-library.add(faServer, faCloudArrowUp, faCloudArrowDown);
-library.add(faLinkedin, faGithubSquare);
+library.add(
+  faCirclePlay,
+  faCloudArrowUp,
+  faCloudArrowDown,
+  faServer,
+);
+library.add(
+  faLinkedin,
+  faGithubSquare
+);
 app.component('font-awesome-icon', FontAwesomeIcon);
 // Routing Config
 app.use(router);
