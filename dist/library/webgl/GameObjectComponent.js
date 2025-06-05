b6e846dec71a08720c7372354b7008c49f026b34
Adam Ettenberger <adam.ettenberger@gmail.com>
2025-06-04 23:22:37 -0700

    Remove dist from main

--- dist/library/webgl/GameObjectComponent.js
+++
@@ -1,7 +0,0 @@
-function GameObjectComponent()
-{
-  this.m_gameObject = null;
-  this.update = function() { /* Update Code */ }
-  this.draw = function(gl) { /* Draw Code */ }
-  this.deserialize = function(xml) { /* Deserialize Data */ }
-}
--- dist/library/webgl/VertexPositionColor.js
+++
@@ -1,8 +0,0 @@
-VertexPositionColor.prototype = new IVertexDeclaration();
-VertexPositionColor.prototype.constructor = VertexPositionColor;
-
-function VertexPositionColor( spPositionPointer, spColorPointer )
-{
-  this.spPositionPointer =  spPositionPointer
-  this.spColorPointer = spColorPointer;
-}
