98beef78f8f54dc6265075477ff37ea1325f56b6
Adam Ettenberger <adam.ettenberger@gmail.com>
2025-06-05 05:40:40 -0700

    Finish converting the RENU page

---
+++ src/components/box.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+div.box {
+  flex: 1;
+  justify-items: stretch;
+  align-content: center;
+  padding: 0 var(--size-padding-round);
+  background-color: var(--color-background-soft);
+  border-radius: var(--size-border-radius);
+  margin: var(--size-padding-round) 0;
+}
+</style>
---
+++ src/components/text_divider.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Box from './box.vue'
+</script>
+
+<template>
+  <div class="divider">
+    <h2 class="heading">
+      <slot></slot>
+    </h2>
+  </div>
+</template>
+
+<style scoped>
+.divider {
+  display: flex;
+  align-items: center;
+  text-align: center;
+  margin: 1lh;
+
+  & .heading {
+    border: 3px solid var(--color-divider);
+    padding: var(--size-padding-round) calc(var(--size-padding-round) * 2);
+    border-radius: var(--size-border-radius);
+  }
+
+  &::before,
+  &::after {
+    content: '';
+    flex: 1;
+    border-bottom: 3px solid var(--color-divider);
+  }
+}
+</style>
