1f80cb897af7943b6603c4e3b5b836ceeb074761
Adam Ettenberger <adam.ettenberger@gmail.com>
2025-06-06 12:12:50 -0700

    Re-exporting tile matching game for web compatibility, should fix mobile browser rendering

--- public/library/projects/tile_match/tile_match.service.worker.js
+++ public/library/projects/tile_match/tile_match.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749021267|911906886';
+const CACHE_VERSION = '1749237054|67704853';
 /** @type {string} */
 const CACHE_PREFIX = 'Tile Match-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
