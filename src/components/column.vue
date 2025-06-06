16af446d6668c42aea9ce20ce554714f1fa57414
Adam Ettenberger <adam.ettenberger@gmail.com>
2025-06-06 05:29:23 -0700

    Revert "Make images fill the column for smaller windows"
    This reverts commit 02f9c39d58e2c0a6a469230e006e093964d96370.

--- src/components/column.vue
+++ src/components/column.vue
@@ -4,7 +4,7 @@
   </div>
 </template>
 
-<style module>
+<style scoped>
 .column {
   display: flex;
   flex-direction: column;
@@ -14,29 +14,5 @@
   background-color: var(--color-background-soft);
   border-radius: var(--size-border-radius);
   margin: var(--size-padding-round) 0;
-
-  & > img,
-  & > svg,
-  & > p > img,
-  & > p > svg {
-    display: block;
-    place-self: center;
-    justify-self: center;
-    align-self: center;
-    border: 6px solid var(--color-divider);
-    margin: var(--size-padding-round) 0;
-    width: 90%;
-  }
-}
-
-@media only screen and (max-width: 740px) {
-  .column {
-    & > img,
-    & > svg,
-    & > p > img,
-    & > p > svg {
-      width: 100%;
-    }
-  }
 }
 </style>
