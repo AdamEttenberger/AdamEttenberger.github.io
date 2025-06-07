b8103e809cb3350f5c5728082da9a66edb57d82d
Adam Ettenberger <adam.ettenberger@gmail.com>
2025-06-06 19:53:57 -0700

    Add Quote component

---
+++ src/components/quote.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  name: { type: String, required: true },
+  cite_url: { type: String, default: null },
+  cite_title: { type: String, default: null },
+})
+</script>
+
+<template>
+  <div>
+    <blockquote :cite="cite_url">
+      <p>&#x275D;<slot></slot>&#x275E;</p>
+    </blockquote>
+    <p class="name">&mdash;{{ name }} <cite v-if="cite_title">{{ cite_title }}</cite></p>
+  </div>
+</template>
+
+<style scoped>
+.name {
+  text-align: right;
+  font-size: smaller;
+  font-style: italic;
+}
+</style>
