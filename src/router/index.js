85d0491d9477247cc9ae1a254da5f0868a7aec02
Adam Ettenberger <Adam.Ettenberger@outlook.com>
2025-06-19 21:27:45 -0700

    Set the router scroll behavior

--- src/router/index.js
+++ src/router/index.js
@@ -76,8 +76,12 @@ const routes = [
     },
   },
 ];
+
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+  scrollBehavior (to, from, savedPosition) {
+    return { top: 0 }
+  },
 })
 export default router
