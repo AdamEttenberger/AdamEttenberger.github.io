6578887aded1ced29a78453e47c3b521b6b23864
Adam Ettenberger <Adam.Ettenberger@outlook.com>
2025-06-22 23:44:22 -0700

    Add numerical input display to number-range
    - Fixup bindings so they automatically coerce to the
    desired type (Number).
    - Add gap around property editor cells

--- src/components/property_editor/number_range.vue
+++ src/components/property_editor/number_range.vue
@@ -15,7 +15,7 @@ const model = defineModel({
   type: Number,
   required: true,
   set(value) {
-    var new_value = Number(value);
+    var new_value = Math.min(Math.max(value, props.min_value), props.max_value);
     emit('property-changed', props.name, new_value);
     return new_value;
   },
@@ -23,15 +23,46 @@ const model = defineModel({
 </script>
 
 <template>
-  <input :name="name" type="range"
-         :min="min_value"
-         :max="max_value"
-         :step="step_value"
-         v-model="model" />
+  <div class="number-range">
+    <input :name="name" type="number"
+           inputmode="decimal"
+           :min="min_value"
+           :max="max_value"
+           :step="step_value"
+           v-model.number.lazy="model" />
+    <input :name="name" type="range"
+           :min="min_value"
+           :max="max_value"
+           :step="step_value"
+           v-model.number="model" />
+  </div>
 </template>
 
 <style scoped>
+.number-range {
+  display: flex;
+  flex-direction: row;
+}
+input[type=number] {
+  font-family: Consolas, Monaco, 'Lucida Console', 'Courier New', Courier, monospace;
+  border-radius: var(--size-border-radius);
+  padding-left: var(--size-padding-round);
+
+  display: block;
+  min-width: 4.5em;
+  transition: background-color var(--anim-transition);
+  background-color: var(--color-background-button);
+  &:hover,
+  &:focus {
+    background-color: var(--color-background-button-hover);
+  }
+  &:active {
+    background-color: var(--color-background-button-active);
+  }
+}
 input[type=range] {
+  flex: 1;
+
   transition: accent-color var(--anim-transition);
   accent-color: var(--color-background-button);
   &:hover,
