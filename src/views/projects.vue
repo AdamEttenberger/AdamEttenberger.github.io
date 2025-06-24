80832966e1dc53446ea18f7d4aae721ecdd239ce
Adam Ettenberger <Adam.Ettenberger@outlook.com>
2025-06-24 16:47:02 -0700

    Begin populating webgl_example.vue

--- src/views/projects.vue
+++ src/views/projects.vue
@@ -57,7 +57,7 @@ import ProjectCard from '@/components/project_card.vue'
     </p>
   </ProjectCard>
 
-  <ProjectCard title="WebGL Example"
+  <ProjectCard title="WebGL Proto-Engine"
                image="/images/projects/webgl-example.png"
                route="/projects/webgl_example"
                :date="new Date('2012/09/01')"
