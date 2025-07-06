6d6a14841e3c6622181c87d4b1c7894179e0cef8
Adam Ettenberger <adam.ettenberger@gmail.com>
2025-07-06 15:59:03 -0700

    Add Match-3 main menu and local score tracking

--- public/library/projects/tile_match/tile_match.service.worker.js
+++ public/library/projects/tile_match/tile_match.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751108925|35743178';
+const CACHE_VERSION = '1751842587|3055549677';
 /** @type {string} */
 const CACHE_PREFIX = 'Tile Match-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
