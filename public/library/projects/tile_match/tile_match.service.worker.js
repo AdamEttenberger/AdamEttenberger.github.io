1baf7834e8ae70831e10980fc9091cc4e746901c
Adam Ettenberger <adam.ettenberger@gmail.com>
2025-07-06 16:08:54 -0700

    [Match-3] Fix NAN scores before first play

--- public/library/projects/tile_match/tile_match.service.worker.js
+++ public/library/projects/tile_match/tile_match.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751842587|3055549677';
+const CACHE_VERSION = '1751843205|143810771';
 /** @type {string} */
 const CACHE_PREFIX = 'Tile Match-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
