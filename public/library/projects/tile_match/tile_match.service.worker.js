5a44c4774a8f316f3313f13196a56b7f4388f53a
Adam Ettenberger <adam.ettenberger@gmail.com>
2025-07-07 08:44:08 -0700

    [Match-3] Add background image and game-over animation + sfx

--- public/library/projects/tile_match/tile_match.service.worker.js
+++ public/library/projects/tile_match/tile_match.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751876480|8775441993';
+const CACHE_VERSION = '1751902988|151796360';
 /** @type {string} */
 const CACHE_PREFIX = 'Tile Match-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
