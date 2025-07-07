ee087b4581b25a77d3ee693ea9e22a8728aed4ad
Adam Ettenberger <Adam.Ettenberger@outlook.com>
2025-07-07 01:55:57 -0700

    [Match-3] onMounted, uninstall Godot service worker

--- src/views/projects/match_three.vue
+++ src/views/projects/match_three.vue
@@ -1,12 +1,18 @@
 <script setup>
+import { onMounted } from 'vue'
 import Player from '@/components/player.vue'
 import UnderConstruction from '@/components/under_construction.vue'
-const props = defineProps({
+defineProps({
   title: { type: String, required: true },
   date: { type: Date, required: true },
   lastmod: { type: Date },
   frame: { type: String, required: true },
 })
+
+onMounted(() => {
+  // Uninstall service workers to fix caching issue when re-visiting the page after updating.
+  navigator.serviceWorker.getRegistrations().then(registrations => registrations.forEach(item => item.unregister()));
+});
 </script>
 
 <template>
