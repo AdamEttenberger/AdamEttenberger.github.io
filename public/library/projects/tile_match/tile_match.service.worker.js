ea5b66918969685535e8f01a8cb7fc7fa5cd51a7
Adam Ettenberger <Adam.Ettenberger@outlook.com>
2025-07-06 21:58:28 -0700

    [Match-3] Add Sfx

--- public/library/projects/tile_match/tile_match.service.worker.js
+++ public/library/projects/tile_match/tile_match.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751844563|541343301';
+const CACHE_VERSION = '1751866777|1504095781';
 /** @type {string} */
 const CACHE_PREFIX = 'Tile Match-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
