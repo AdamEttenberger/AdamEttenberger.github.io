9ae0bb9889b3caaf91e8223f330c055c50075aca
Adam Ettenberger <Adam.Ettenberger@outlook.com>
2025-07-07 00:45:02 -0700

    [Match-3] Adjust match sound to score

--- public/library/projects/tile_match/tile_match.service.worker.js
+++ public/library/projects/tile_match/tile_match.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751866777|1504095781';
+const CACHE_VERSION = '1751874183|6478084393';
 /** @type {string} */
 const CACHE_PREFIX = 'Tile Match-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
