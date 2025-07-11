2c1d2935b06545a2b48ccb13eff2b19301e09e9a
Adam Ettenberger <adam.ettenberger@gmail.com>
2025-07-10 20:22:24 -0700

    [Match-3] Update game, small improvements

--- public/library/projects/tile_match/tile_match.service.worker.js
+++ public/library/projects/tile_match/tile_match.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1752157638|18366659889';
+const CACHE_VERSION = '1752204013|2730083399';
 /** @type {string} */
 const CACHE_PREFIX = 'Tile Match-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
