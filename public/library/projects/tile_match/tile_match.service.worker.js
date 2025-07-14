d85a0d4f27be72ddffb30617e6c89c602d3e8580
Adam Ettenberger <Adam.Ettenberger@outlook.com>
2025-07-14 09:03:20 -0700

    [Match-3] Some bug fixes, better audio scaling

--- public/library/projects/tile_match/tile_match.service.worker.js
+++ public/library/projects/tile_match/tile_match.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1752499636|15686177892';
+const CACHE_VERSION = '1752508887|6979118698';
 /** @type {string} */
 const CACHE_PREFIX = 'Tile Match-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
