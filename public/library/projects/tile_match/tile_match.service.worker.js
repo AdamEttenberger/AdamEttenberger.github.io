28b910add149e08b4bd4be068ebf6618ad660e83
Adam Ettenberger <adam.ettenberger@gmail.com>
2025-07-14 06:28:31 -0700

    [Match-3] More UI improvements, score particle effects

--- public/library/projects/tile_match/tile_match.service.worker.js
+++ public/library/projects/tile_match/tile_match.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1752484088|138054421';
+const CACHE_VERSION = '1752499636|15686177892';
 /** @type {string} */
 const CACHE_PREFIX = 'Tile Match-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
