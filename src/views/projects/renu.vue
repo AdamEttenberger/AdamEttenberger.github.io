454a1701cc60cee0b873a010adf8010532436066
Adam Ettenberger <adam.ettenberger@gmail.com>
2025-07-15 22:02:32 -0700

    [Renu] Mention the floaty jump feature, some formatting

--- src/views/projects/renu.vue
+++ src/views/projects/renu.vue
@@ -43,6 +43,7 @@ const props = defineProps({
         Amanda created all of the art and animations for the game, Nate created all of the music and sound effects, and Josh mostly helped design the game mechanics and experience as well as some programming.
         As for myself, I did nearly all of the programming and I designed each of the levels.
       </p>
+      <br />
       <Figure class="team"
               src="/images/projects/renu/team.jpg"
               alt="Profile pictures of the team members.">
@@ -65,6 +66,7 @@ const props = defineProps({
         This allowed us to quickly create, modify, and transition between maps without having to write out each individual location or learning a new tool.
         We were able to create maps using MS-Paint.
       </p>
+      <br />
       <Figure src="/images/projects/renu/levelSample.png"
               alt="Example of the bitmap level file used in the previous game screen image." />
       <Figure src="/images/projects/renu/gameplay.png"
@@ -77,6 +79,7 @@ const props = defineProps({
         Additionally, we decided that instead of having health or lives, we would allow the player to continue from the beginning of the level as many times as they would like.
         This not only reduced some development time, but also in my opinion made the game less frustrating if you ended up dying a lot on a certain level.
       </p>
+      <br />
       <Figure src="/images/projects/renu/death_animation.gif"
               alt="Animation illustrating the physics-based player death animation." />
     </Section>
@@ -89,12 +92,21 @@ const props = defineProps({
         One thing the player will notice immediately about walking on the cleaned patches versus the grimy patches is that friction is much higher on the clean paths.
         This causes the player to slow down much faster when they are not trying to move in any direction, which in turn makes some jumps easier to make.
       </p>
+      <br />
       <Figure src="/images/projects/renu/slippery_tiles.gif"
               alt="Animation demonstrating how one tile type has lass friction, slippery movement." />
       <Figure src="/images/projects/renu/sticky_tiles.gif"
               alt="Animation demonstrating how one tile type has more friction, sticky movement." />
       <Figure src="/images/projects/renu/tile_difference.gif"
               alt="Animation demonstrating the difference in friction between the two terrain tile types." />
+      <br />
+      <p>
+        Another subtle feature is the floaty-jump, which makes the slippery platforming a more forgiving.
+        Players are allowed 1 jump action which resets after the touching the ground.
+      </p>
+      <br />
+      <Figure src="/images/projects/renu/floaty_jump.gif"
+              alt="Animation demonstrating a player jumping in mid-air after walking off a platform." />
     </Section>
 
     <Section heading="Boss Fight">
