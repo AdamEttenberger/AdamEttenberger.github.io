aa65f0f66b57d43bb005128d357788b5c594a4ef
Adam Ettenberger <adam.ettenberger@gmail.com>
2025-07-23 10:43:17 -0700

    Cleanup useMatchThreeScorecardStore

--- src/views/projects/match_three.vue
+++ src/views/projects/match_three.vue
@@ -1,7 +1,10 @@
-<script setup>
+<script setup lang="ts">
 import { onMounted } from 'vue'
 import Player from '@/components/player.vue'
 import UnderConstruction from '@/components/under_construction.vue'
+import { useMatchThreeScorecardStore } from '@/stores/match_three_scorecard'
+const scorecard = useMatchThreeScorecardStore();
+
 defineProps({
   title: { type: String, required: true },
   date: { type: Date, required: true },
@@ -10,7 +13,8 @@ defineProps({
 })
 
 onMounted(() => {
-  // Uninstall service workers to fix caching issue when re-visiting the page after updating.
+  // Uninstall service workers to fix caching issue when re-visiting the page
+  // after the game had been updated on the server.
   navigator.serviceWorker.getRegistrations().then(registrations => registrations.forEach(item => item.unregister()));
 });
 </script>
