c016c47b7543a32f8e13b28165a07c86b47922a6
Adam Ettenberger <adam.ettenberger@gmail.com>
2025-07-23 18:10:34 -0700

    Remove unused method

--- src/stores/consent.ts
+++ src/stores/consent.ts
@@ -7,20 +7,11 @@ export const useConsentStore = defineStore('consent', () => {
   const allow_saving_user_preferences = ref(false);
   const allow_saving_match_three_scorecard = ref(false);
 
-  function toggleConsent(key) {
-    if (this[key] === undefined) {
-      return;
-    }
-    this[key].value = !this[key].value
-  }
-
   return {
     // State
     allow_hiding_consent_banner,
     allow_first_party_tracking,
     allow_saving_user_preferences,
     allow_saving_match_three_scorecard,
-    // Actions
-    toggleConsent,
   };
 });
