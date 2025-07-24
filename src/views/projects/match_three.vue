0530daac637193b8bc919006a878d4c8383fe3a8
Adam Ettenberger <adam.ettenberger@gmail.com>
2025-07-24 12:17:40 -0700

    Update Godot bridge and scorecard synchronization

--- src/views/projects/match_three.vue
+++ src/views/projects/match_three.vue
@@ -17,12 +17,31 @@ onMounted(() => {
   // after the game had been updated on the server.
   navigator.serviceWorker.getRegistrations().then(registrations => registrations.forEach(item => item.unregister()));
 });
+
+function bindGodotBridge(frame) {
+  addEventListener('message', (event) => {
+    if (event.origin !== window.location.origin) {
+      return;
+    }
+    switch (event.data.type) {
+      case 'ready':
+        if (scorecard.scorecard) {
+          frame.contentWindow.postMessage({'scorecard': JSON.stringify(scorecard.scorecard)}, window.location.origin);
+        }
+        break;
+      case 'scorecard':
+        scorecard.scorecard = JSON.parse(event.data.scorecard);
+        break;
+    }
+  });
+}
 </script>
 
 <template>
   <Player :title="title"
           :date="date"
           :lastmod="lastmod"
-          :frame="frame" />
+          :frame="frame"
+          @load="bindGodotBridge" />
   <UnderConstruction />
 </template>
