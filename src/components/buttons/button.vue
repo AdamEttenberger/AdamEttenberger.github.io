dc97b0ef239c2724bf47ed58bffde9d674113d8a
Adam Ettenberger <adam.ettenberger@gmail.com>
2026-02-08 02:07:07 -0800

    Update <Button> to use <Layer>

--- src/components/buttons/button.vue
+++ src/components/buttons/button.vue
@@ -1,137 +1,86 @@
 <script setup lang="ts">
-import { unref, PropType } from 'vue'
-import ExternalLink from '@/components/external_link.vue'
-import EmailTemplate from '@/types/email_template';
+import { computed, PropType, unref } from 'vue'
+import Link from '@/components/link.vue'
+import EmailTemplate from '@/types/email_template'
+import Layer from '@/components/layer.vue'
+import { ThemeColor } from '@/composables/theme'
 
 defineEmits(['click']);
 
-defineProps({
-  src: { type: String, default: null },
-  icon: { type: Array, default: null },
-  text: { type: String, defeault: null },
-  alt: { type: String, default: null },
-  route: { type: String, default: null },
-  to: { type: String, default: null },
-  mailto: { type: EmailTemplate, default: null },
-  disabled: { type: [Boolean, Object] as PropType<Boolean | Object>, default: false },
+const props = defineProps({
+  color: { type: [null, String, ThemeColor] as PropType<null|String|ThemeColor>, default: null },
   transparent: { type: Boolean, default: false },
+
+  to: { type: [String, EmailTemplate] as PropType<String|EmailTemplate>, default: null },
+  alt: { type: String, default: null },
+  icon: { type: [null, String, Array] as PropType<null|String|Array>, default: null },
+  text: { type: String, default: null },
+  disabled: { type: [Boolean, Object] as PropType<Boolean|Object>, default: false },
 })
+const tabindex = computed(() => unref(props.disabled) ? -1 : undefined);
 </script>
 
 <template>
-  <button class="image_button" @click="$emit('click')" :disabled="unref(disabled)">
-    <RouterLink v-if="route" class="router" :to="route">
-      <div :class="{'animator': true, 'transparent': transparent}">
-        <slot v-if="$slots.default"></slot>
-        <img v-else-if="src" :src="src" :alt="alt" />
-        <font-awesome-icon v-else-if="icon" :icon="icon" class="image-button-fa-icon" />
-        <div v-else-if="text" class="text">{{ text }}</div>
-      </div>
-    </RouterLink>
-    <ExternalLink v-else-if="to ?? mailto" class="link" :to="to ?? mailto?.toString()" :show_ext="false">
-      <div :class="{'animator': true, 'transparent': transparent}">
-        <slot v-if="$slots.default"></slot>
-        <img v-else-if="src" :src="src" :alt="alt" />
-        <font-awesome-icon v-else-if="icon" :icon="icon" class="image-button-fa-icon" />
-        <div v-else-if="text" class="text">{{ text }}</div>
-        <div v-else-if="mailto" class="text">{{ mailto.address }}</div>
-      </div>
-    </ExternalLink>
-    <div v-else :class="{'animator': true, 'transparent': transparent}">
-      <slot v-if="$slots.default"></slot>
-      <img v-else-if="src" :src="src" :alt="alt" />
-      <font-awesome-icon v-else-if="icon" :icon="icon" class="image-button-fa-icon" />
-      <div v-else-if="text" class="text">{{ text }}</div>
-    </div>
-  </button>
+  <Layer :class="['button-background-layer', disabled?'disabled':'']" :color :transparent show_hover>
+    <Link class="button-link" :to :icon :alt :disabled hide_ext button :tabindex @click="$emit('click', $event)">
+      <template v-if="$slots.default || text" #default>
+        <slot>{{ text }}</slot>
+      </template>
+    </Link>
+  </Layer>
 </template>
 
 <style scoped>
-.image_button {
-  display: block;
-  align-self: center;
-  border: none;
-  padding: 0;
-  margin: 0;
-  background-color: transparent;
-  font-size: 1.2rem;
-}
-
-.image_button:not([disabled]) {
+.button-background-layer {
+  /**
+   * Default behavior is to act similar to an intrinsic <button>; centering <slot/>
+   * contents both vertically and horizontally, and sizing laying out <slot/>
+   * contents sized to fit-content (i.e., doesn't stretch to fill the <Button>).
+   * i.e., approx. the child box doesn't automatically fill the button.
+   * To make the default behavior predictable and easy to override by callers.
+   */
+  display: inline-flex;
+  text-align: center;
   cursor: pointer;
-}
+  box-sizing: border-box;
+  letter-spacing: normal;
+  word-spacing: normal;
+  line-height: normal;
+  text-rendering: auto;
+  text-transform: none;
+  text-indent: 0px;
+  text-shadow: none;
+  user-select: none;
 
-.animator {
-  color: var(--color-text-button);
-}
-
-.image_button > .router,
-.image_button > .link {
-  display: contents;
-}
-
-.animator {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-.image_button,
-.animator,
-.animator > img {
-  align-content: center;
-  width: 100%;
-  height: 100%;
-}
-
-.animator,
-.animator > :is(img, svg, :slotted(img), :slotted(svg)) {
-  border-radius: var(--size-border-radius);
-}
-
-.animator:not(.transparent) {
-  transition: background-color var(--anim-transition);
-  background-color: var(--color-background-button);
-  &:hover {
-    background-color: var(--color-background-button-hover);
+  &.disabled {
+    pointer-events: none;
   }
-  &:active {
-    background-color: var(--color-background-button-active);
-  }
-  &:disabled,
-  [disabled] & {
-    background-color: var(--color-background-button-disabled);
-  }
-}
 
-.delete .animator:not(.transparent) {
-  transition: background-color var(--anim-transition);
-  background-color: var(--color-background-error);
-  color: var(--color-text-error);
-  &:hover {
-    background-color: --button-hover(var(--color-background-error));
-  }
-  &:active {
-    background-color: --button-active(var(--color-background-error));
-  }
-  &:disabled,
-  [disabled] & {
-    background-color: --button-disabled(var(--color-background-error));
-  }
-}
+  & > .button-link {
+    /**
+     * Default behavior behaves similar to the intrinsic <button>.
+     * The <Link> is made to fit the containing <Layer> so the entire area of the
+     * <Layer> is clickable, delegating activation logic to <Link>.
+     * However, the <slot/> contents inside should be centered and "fit-content".
+     * i.e., approx. the child box doesn't automatically fill the button.
+     * This makes the default behavior predictable and easy to override by callers.
+     */
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: inherit;
+    background-color: inherit;
+    font-size: inherit;
+    &:not([disabled]) { cursor: pointer; }
 
-.animator:has(*.image-button-fa-icon) {
-  aspect-ratio: 1;
-}
-
-.image-button-fa-icon {
-  font-size: 1.4rem;
-}
-
-.text {
-  height: 2rem;
-  align-content: center;
-  padding: 0 var(--size-padding-round);
+    /* See comment in '@/components/layer.vue' for details. */
+    margin: var(--inverse-component-layer-padding);
+    /**
+     * Add a 'normal' amount of padding to push the contents in from the
+     * edge of the button. This should work for most used cases.
+     */
+    padding: var(--padding-normal);
+  }
 }
 </style>
