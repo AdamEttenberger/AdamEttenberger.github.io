3a96329c8236ed9a9f3a086a1364491e1d41d74c
Adam Ettenberger <adam.ettenberger@gmail.com>
2026-02-08 04:06:46 -0800

    Small updates to property button/divider

--- src/components/property_editor/property_divider.vue
+++ src/components/property_editor/property_divider.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
 import Divider from '@/components/divider.vue'
 import { IPropertyDividerOptions } from '@/util/property_editor/property_interfaces';
-
 defineProps<IPropertyDividerOptions>();
 </script>
 
 <template>
   <Divider class="property-editor-divider"
-           :text="label" />
+           :heading="label" />
 </template>
 
 <style scoped>
 .property-editor-divider {
-  margin: 0;
+  padding: 0;
+
   & :deep(.heading) {
     border: none;
     font-size: medium;
