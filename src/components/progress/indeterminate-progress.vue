1f0201b88af953fa5a6c564071c5a2c9a6ad0e1f
Adam Ettenberger <adam.ettenberger@gmail.com>
2026-03-02 05:45:57 -0800

    Add animated IndeterminateProgress component

---
+++ src/components/progress/indeterminate-progress.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="progress-container">
+    <font-awesome-icon class="fa-icon spinner" :icon="['fas', 'compass']" />
+  </div>
+</template>
+
+<style scoped>
+@keyframes spinner {
+  from { rotate: 0deg; }
+  to { rotate: 360deg; }
+}
+.progress-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background: repeating-linear-gradient(
+      45deg,
+      transparent,
+      transparent 10px,
+      var(--theme-background-shade) 10px,
+      var(--theme-background-shade) 20px
+  );
+
+  & .spinner {
+    font-size: 5rem;
+    padding: var(--padding-normal);
+    animation: 2s infinite spinner;
+    background-color: var(--theme-background-shade);
+    clip-path: circle(50%);
+  }
+}
+</style>
