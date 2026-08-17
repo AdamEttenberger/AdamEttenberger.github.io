21b09c213c8d64dc9423f70d0943ace9a9746848
Adam Ettenberger <adam.ettenberger@gmail.com>
2026-08-17 19:29:43 -0400

    Add WebGPU Logo and license entry for attribution

---
+++ src/components/webgpu-logo.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import webgpu_logo from '@/assets/images/logos/webgpu/webgpu-horizontal-responsive.svg'
+</script>
+
+<template>
+  <img alt="WebGPU logo by W3C"
+       :src="webgpu_logo" />
+</template>
