2cfd36d4ec007622c332b3c6af78cd34698adcb4
Adam Ettenberger <adam.ettenberger@gmail.com>
2026-09-01 08:51:23 -0400

    Add 2 more ocean instances

--- src/components/hero/hero-section-webgpu.vue
+++ src/components/hero/hero-section-webgpu.vue
@@ -19,7 +19,7 @@ const kAnimationGridSize = vec2.fromValues(8, 8); // Number of animation frame [
 
 const kOceanGridSize = 50;
 const kInstanceTileScale = vec3.fromValues(600, 200, 600);
-const kInstanceTileArea = vec4.fromValues(-1, -1, 1, 0);
+const kInstanceTileArea = vec4.fromValues(-2, -1, 2, 0);
 
 const kOceanAlbedo: Vec3 = vec3.fromValues(0.0, 0.425, 0.725);
 
